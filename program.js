b5bd862f0def67fd78b97cdb2d3e377fc92f3ace
Fábio Gusmão <fabio.gusmao@qx3.com.br>
2017-04-11 00:52:33 -0300

    #1 @raphaelquati Disponibilizei a possibilidade de usar o S3 como armazenamento para os backups. Atente que a estrutura do arquivo de configuração foi bem modificada e agora o programa utiliza o SDK do Firebase ao invés de simplesmente tentar baixar o JSON por uma URL, o que agora possibilita o uso de credenciais de autenticação.

--- program.js
+++ program.js
@@ -1,98 +1,80 @@
 var fs = require('fs');
-var https = require('https');
 var path = require('path');
 
+var admin = require('firebase-admin');
+
+
+var S3Device = require('./s3');
+var FileSysDevice = require('./filesys');
 
 const FB_CONFIG_FILE = "config.json";
 
 var globalConfig = null;
 
+var device;
+
 console.log("### Firebase Backupp Agent ###");
 
 function loadConfig() {
-    
-     try{
+
+    try {
         fs.accessSync(FB_CONFIG_FILE, fs.F_OK);
-    }catch(e){
+    } catch (e) {
         console.log("The configuration file '" + FB_CONFIG_FILE + "' has not been created yet! See README file for instructions.");
         process.exit(-1);
-    }    
-    
-    
+    }
+
+
     console.log("Loading configuration from '" + FB_CONFIG_FILE + "'...");
     globalConfig = JSON.parse(fs.readFileSync(FB_CONFIG_FILE, 'utf8'));
     console.log("Configuration loaded!");
+
+    switch (globalConfig.dest.type) {
+        case "filesys": device = new FileSysDevice(globalConfig.dest); break;
+        case "s3": device = new S3Device(globalConfig.dest); break;
+        default: console.log("Unsupported destination type"); exit(-1);
+    }
 }
 
 
 function getLatestVersion(success, failure) {
-    var body = "";
-    console.log('Downloading JSON ...');
-    var req = https.get(globalConfig.url, function (res) {
-        var len = res.headers["content-length"];
-        console.log('Receiving ' + len + ' bytes...');
-        res.setEncoding('utf8');
-        res.on('data', function (chunk) {
-            body+=chunk;            
-        });
-        res.on('end', function(){
-            console.log('Download complete!');
-            success(body);
-        });
-    });
 
-    req.on('error', function (e) {
-        console.error(e);
+    var db = admin.database();
+
+    var ref = db.ref('/');
+
+    ref.once('value', function (snapshot) {
+        var json = JSON.stringify(snapshot.val());
+        success(json);
     });
 }
-function getSnapshotFilename(tag){
-    var filename = globalConfig.dest.pattern + tag + '.json';
-    var filePath = path.join(globalConfig.dest.folder, filename);
-    
-    return filePath;
+
+
+function loadSnapshot(tag, success, error) {
+    device.getSnapshot(tag, success, error);
 }
 
-function loadSnapshot(tag){
-    var filePath = getSnapshotFilename(tag);
-    try{
-        fs.accessSync(filePath, fs.F_OK);
-    }catch(e){
-        console.log('"' + filePath + '" does not exist.');
-        return false;
-    }    
-    
-    console.log('Loading snapshot from "' + filePath + '"...');
-    var snapshot = fs.readFileSync(filePath, 'utf8');
-    return snapshot;
+function compareWithLatesst(json, success, error) {
+    loadSnapshot('latest', function (latest) {
+        if (latest === false) {
+            success(false);
+            return;
+        }
+        var equal = json === latest;
+        if (equal) {
+            console.log("No changes!");
+        } else {
+            "JSON changed!";
+        }
+        success(equal);
+    }, error);
 }
 
-function compareWithLatesst(json){
-    var latest = loadSnapshot('latest');
-    if(latest === false)
-        return false;
-    var equal = json === latest;
-    if(equal){
-        console.log("No changes!");
-    }else{
-        "JSON changed!";
-    }
-    return equal;
-    
+function saveSnapshot(json, tag, success, error) {
+    device.saveSnapshot(json, tag, success, error);
 }
 
-function saveSnapshot(json, tag){
-    var filePath = getSnapshotFilename(tag);
-    console.log('Saving JSON to "' + filePath + '"...');
-    try{
-        fs.accessSync(globalConfig.dest.folder, fs.F_OK);
-    }catch(e){
-        fs.mkdirSync(globalConfig.dest.folder);    
-    }
-    
-    fs.writeFileSync(filePath, json);
-}
-
-function createTimestampTag(){
+function createTimestampTag() {
     var d = new Date;
     var tag = d.toISOString();
     tag = tag.replace(/(([\:-])|(\..*$))/g, '');
@@ -102,13 +84,31 @@ function createTimestampTag(){
 function main() {
 
     loadConfig();
-    getLatestVersion(function (json) {
-        if(!compareWithLatesst(json)){
-            var tag = createTimestampTag();
-            saveSnapshot(json, tag);
-            saveSnapshot(json, 'latest');
-        }
+
+    var admin = require("firebase-admin");
+
+    var serviceAccount = require(globalConfig.firebase.credentialsFile);
+
+    admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount),
+        databaseURL: globalConfig.firebase.databaseURL
     });
 
+    getLatestVersion(function (json) {
+
+        compareWithLatesst(json, function (equal) {
+            if (!equal) {
+                var tag = createTimestampTag();
+                saveSnapshot(json, tag, function () {
+                    saveSnapshot(json, 'latest', function () {
+                        console.log('Complete!');
+                        process.exit(0);
+                    });
+                });
+            }else{
+                process.exit(0);
+            }
+        });
+    });
 }
 main();
